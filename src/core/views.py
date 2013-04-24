ee8fc197135aa0530a5c2e9f5af39ee3bf564366
Sandeep Raju <sandeep080@gmail.com>
2013-04-24 22:58:27 +0530

    added error handlers

--- src/core/views.py
+++ src/core/views.py
@@ -7,8 +7,26 @@ import helpers as util
 import json
 
 # Third party imports
-from flask import render_template
+from flask import render_template, jsonify, request
 
+# Application homepage
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# Application error handlers
+@app.errorhandler(400)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(405)
+def not_found(error=None):
+    message = {
+            'code': error.code,
+            'status': 'failure',
+            'url': request.url,  # set the requested url
+            'reason': error.name
+    }
+    resp = jsonify(message)  # generate a response object
+    resp.status_code = error.code  # set the error status code
+    return resp
