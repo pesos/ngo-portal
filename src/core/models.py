3cab091be9232542bad447d5b33a1e4e6581373b
Akshay M S <akshayms91@gmail.com>
2013-05-11 17:18:45 +0530

    Changed backrefs and added user-organization relations.

--- src/core/models.py
+++ src/core/models.py
@@ -15,6 +15,16 @@ user_focus = db.Table('user_focus',
     db.Column('focus_id', db.Integer, db.ForeignKey('focus.id'))
 )
 
+org_focus = db.Table('org_focus',
+    db.Column('org_id', db.Integer, db.ForeignKey('organisation.id')),
+    db.Column('focus_id', db.Integer, db.ForeignKey('focus.id'))
+)
+
+user_org = db.Table('user_org',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('org_id', db.Integer, db.ForeignKey('organisation.id'))
+)        
+
 class User(db.Model):
     """ `User` is any entity which uses the application. User will
     have a valid facebook account to access the application.
@@ -41,33 +51,27 @@ class User(db.Model):
     blood = db.Column(db.Boolean)
     organ = db.Column(db.Boolean)
     disaster = db.Column(db.Boolean)
-    focus = db.relationship('Focus', secondary=user_focus, backref=db.backref('users', lazy='dynamic'))
-	
+    focus = db.relationship('FocusAreas', secondary=user_focus)
+    orgs = db.relationship('Organisation', secondary=user_org)
     def __repr__(self):
-        return "<User:%r:%r>" % (self.id , self.name,self.email,self.role)
-
-org_focus = db.Table('org_focus',
-    db.Column('org_id', db.Integer, db.ForeignKey('organisation.id')),
-    db.Column('focus_id', db.Integer, db.ForeignKey('focus.id'))
-)
+        return "<User:%d:%s:%s:%s>" % (self.id , self.first_name,self.email,self.last_name)
 
 class Organisation(db.Model):
     """Organisation` is any entity which uses the application."""
     __tablename__ = "organisation"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    yer_es=db.Column(db.Integer(255),nullable=False)
-    focus=db.Column(db.String(255),nullable=False)
-    about=db.Column(db.String(255),nullable=False)
-    reg_adr=db.Column(db.String(255),nullable=False)
-    cor_adr=db.Column(db.String(255),nullable=False)
-    contact_name=db.Column(db.String(255),nullable=False)
-    contact_des=db.Column(db.String(255),nullable=False)
-    icare_phone=db.Column(db.Integer,nullable=False)
-    icare_mobile=db.Column(db.Integer,nullable=False)
-    icare_fax=db.Column(db.Integer,nullable=False)
-    website = db.Column(db.String(255),nullable=False)
-    icare_email = db.Column(db.String(255),nullable=False)
+    yer_es=db.Column(db.Integer(255),nullable=True)
+    about=db.Column(db.String(255),nullable=True)
+    reg_adr=db.Column(db.String(255),nullable=True)
+    cor_adr=db.Column(db.String(255),nullable=True)
+    contact_name=db.Column(db.String(255),nullable=True)
+    contact_des=db.Column(db.String(255),nullable=True)
+    icare_phone=db.Column(db.Integer,nullable=True)
+    icare_mobile=db.Column(db.Integer,nullable=True)
+    icare_fax=db.Column(db.Integer,nullable=True)
+    website = db.Column(db.String(255),nullable=True)
+    icare_email = db.Column(db.String(255),nullable=True)
     social = db.Column(db.Boolean)
     skills_based = db.Column(db.Boolean)
     micro = db.Column(db.Boolean)
@@ -78,17 +82,17 @@ class Organisation(db.Model):
     dis_stationary=db.Column(db.Boolean)
     dis_software=db.Column(db.Boolean)
     dis_hardware=db.Column(db.Boolean)
-    focus = db.relationship('Focus', secondary=org_focus, backref=db.backref('organisations', lazy='dynamic'))
+    focus = db.relationship('FocusAreas', secondary=org_focus)
 
     def __repr__(self):
-        return "<Organisation:%r:%r>" % (self.id , self.name,self.website,self.email)
+        return "<Organisation:%d:%s:%s:%s>" % (self.id , self.name,self.website,self.email)
 
 
 class FocusAreas(db.Model):
     """'Focus Areas' is an entity which has focus areas as fields"""
     __tablename__ = "focus"
     id = db.Column(db.Integer,primary_key=True)
-    interest_name=db.Column(db.Integer,nullable=False)
+    interest_name=db.Column(db.String(255),nullable=False)
 
     def __repr__(self):
           return "<FocusAreas:%r:%r>" % (self.id,self.interest_name)
